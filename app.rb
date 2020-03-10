f5fc1604dd9be50b280ff4b2ff06ff7f2933b2cd
ajj1565 <alex.jones@kellogg.northwestern.edu>
2020-03-10 01:23:59 +0000

    integrate twilio

--- app.rb
+++ app.rb
@@ -5,6 +5,10 @@ require "sequel"                                                                
 require "logger"                                                                      #
 require "twilio-ruby"                                                                 #
 require "bcrypt"                                                                      #
+require "geocoder"
+account_sid = AC0c9a0670fb6c0165b8f10fdd1797a49f
+auth_token = d29cd2956ee05d29cbd72136a70e8ebe
+client = Twilio::REST::Client.new(account_sid, auth_token)
 connection_string = ENV['DATABASE_URL'] || "sqlite://#{Dir.pwd}/development.sqlite3"  #
 DB ||= Sequel.connect(connection_string)                                              #
 DB.loggers << Logger.new($stdout) unless DB.loggers.size > 0                          #
@@ -13,6 +17,12 @@ use Rack::Session::Cookie, key: 'rack.session', path: '/', secret: 'secret'     
 before { puts; puts "--------------- NEW REQUEST ---------------"; puts }             #
 after { puts; }                                                                       #
 #######################################################################################
+
+client.messages.create(
+  from: "+17724105382", 
+  to: "+16084383038",
+  body: "Hey KIEI 451!"
+)
 
 events_table = DB.from(:events)
 rsvps_table = DB.from(:rsvps)
